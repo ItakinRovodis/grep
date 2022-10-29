cc6785b5ff930edc3d0ea5e4061ca2137c3307bf
ItakinRovodis <ItakinRovodis@gmail.com>
2022-10-29 12:46:37 +0300

    first step

--- grep.c
+++ grep.c
@@ -8,11 +8,13 @@ int main(int argc, char **argv) {
 	int eflag = 0, iflag = 0, vflag = 0, cflag = 0, lflag = 0, nflag = 0, hflag = 0, sflag = 0, fflag = 0, oflag = 0;
 	int opt;
 	char * targetString;
+	int targetStringIndecies[10];
 
 	while ((opt = getopt(argc, argv, "eivclnhsfo?")) != -1) {
 		switch (opt) {
 			case 'e':
-				eflag = 1;
+				targetStringIndecies[eflag] = optind;
+				eflag++;
 				break;
 			case 'i':
 				iflag = 1;
@@ -50,14 +52,13 @@ int main(int argc, char **argv) {
 	FILE *fp;
 	const int bufferSize = 4096;
 	char buffer[bufferSize];
-	int currentFile = optind+1;
+	int currentFile = optind+2;
 	targetString = argv[optind];
 	while (currentFile < argc) {
 		if (argc < 3) {
 			printf("usage: grep [option] [-eivclnhsfo] [file ...]\n");
 			exit(1);
-		} else {			
-			printf("%s %d\n", argv[currentFile], currentFile);
+		} else {
 			fp = fopen(argv[currentFile], "rb");
 			if (fp == NULL) {
 				fprintf(stderr, "%s: %s : No such file of directory\n",
@@ -65,14 +66,33 @@ int main(int argc, char **argv) {
 				exit(1);
 			}
 		}
-
-		while (fgets(buffer, bufferSize, fp)) {
-			int length = strlen(buffer);
-			buffer[length-1] = '\0';
-			if (strstr(buffer, targetString) != NULL) {
-				fprintf(stdout, "%s\n", buffer);
+		if (eflag > 0) {
+			for (int i = 0; i < eflag; ++i) {
+				if (i == 0) {
+					targetString = argv[targetStringIndecies[i]+1];
+				} else {
+					targetString = argv[targetStringIndecies[i]];
+				}
+				while (fgets(buffer, bufferSize, fp)) {
+					int length = strlen(buffer);
+					buffer[length-1] = '\0';
+					if (strstr(buffer, targetString) != NULL) {
+						fprintf(stdout, "%s\n", buffer);
+					}
+				}
+				fclose(fp);
+				fp = fopen(argv[currentFile], "rb");
 			}
-		}
+			
+		} else {
+			while (fgets(buffer, bufferSize, fp)) {
+				int length = strlen(buffer);
+				buffer[length-1] = '\0';
+				if (strstr(buffer, targetString) != NULL) {
+					fprintf(stdout, "%s\n", buffer);
+				}
+			}
+		}		
 		fclose(fp);
 		currentFile++;
 	}
